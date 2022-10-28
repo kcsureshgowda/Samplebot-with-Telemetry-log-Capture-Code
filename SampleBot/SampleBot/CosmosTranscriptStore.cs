04ed3a050a0ccc62b5065c28f3e4ec77d12b2615
Suresh K C <su268302@wipro.com>
2022-10-28 13:31:11 +0530

    Core bot with sample code to capture Telemetry data into Cosmos DB

---
+++ SampleBot/SampleBot/CosmosTranscriptStore.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleBot
+{
+    public class CosmosTranscriptStore : ITranscriptLogger
+    {
+        //variable to hold CosmosDB storage 
+        private CosmosDbPartitionedStorage _cosmosDBStorage;
+
+        //Constructor to initilize the CosmosDB storage
+        public CosmosTranscriptStore(CosmosDbPartitionedStorageOptions dbStorageOption)
+        {
+            _cosmosDBStorage = new CosmosDbPartitionedStorage(dbStorageOption);
+        }
+
+        //Method to log the telemetry data into CosmosDB
+        public async Task LogActivityAsync(IActivity activity)
+        {
+
+            // Validate that activity object is null or it has data
+            var isMessage = activity.AsMessageActivity() != null ? true : false;
+            if (isMessage)
+            {
+                // Customize data before store into CosmosDB
+                var data = new
+                {
+                    Activity = activity
+                };
+                var document = new Dictionary<string, object>();
+                // activity.Id is being used as the Cosmos Document Id
+                document.Add(activity.Id, data);
+                await _cosmosDBStorage.WriteAsync(document, new CancellationToken());
+            }
+        }
+
+    }
+}
